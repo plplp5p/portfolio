6534182d220d1f2f977069357aa5f386c59b0433
sarahalwadi <sarahalwadi5566@gmail.com>
2026-05-31 09:19:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -61,3 +61,10 @@ function deleteEffect(){
 }
 
 typeEffect();
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
